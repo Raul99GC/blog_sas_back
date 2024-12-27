b005a4fe141191772fc8b2746e67faa3a7ff46a5
Raul99GC <raulc99g@gmail.com>
2024-12-27 14:25:06 -0600

    Annotate UserService with @Service for dependency injection

--- src/main/java/com/raulcg/blog/services/user/UserService.java
+++ src/main/java/com/raulcg/blog/services/user/UserService.java
@@ -6,9 +6,11 @@ import com.raulcg.blog.models.User;
 import com.raulcg.blog.repositories.UserRepository;
 import com.raulcg.blog.request.SignupRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class UserService implements IUserService {
 
     @Autowired
