ebf93eeadd7c3f002573227beb9b8ae049d6dd89
Raul99GC <raulc99g@gmail.com>
2024-12-27 10:31:35 -0600

    Add methods to UserRepository for email and username operations

--- src/main/java/com/raulcg/blog/repositories/UserRepository.java
+++ src/main/java/com/raulcg/blog/repositories/UserRepository.java
@@ -3,7 +3,16 @@ package com.raulcg.blog.repositories;
 import com.raulcg.blog.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+
+    public boolean existsByEmail(String email);
+
+    public boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 }
