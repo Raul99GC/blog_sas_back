c2f4d94d32aa822920217005856e5fa731743893
Raul99GC <raulc99g@gmail.com>
2024-12-29 16:58:52 -0600

    Add Role with one-to-many relationship with User

---
+++ src/main/java/com/raulcg/blog/models/Role.java
@@ -0,0 +1,38 @@
+package com.raulcg.blog.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.raulcg.blog.UserRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    UUID id;
+
+    @Column(name = "role_name", length = 20)
+    @Enumerated(EnumType.STRING)
+    @ToString.Exclude
+    UserRole roleName;
+
+    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
+    @JsonBackReference
+    @ToString.Exclude
+    private Set<User> users = new HashSet<>();
+
+    public Role(UserRole roleName) {
+        this.roleName = roleName;
+    }
+}
