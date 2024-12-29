fc9235378a0ef0ec9fef96b213cea6c58456c14a
Raul99GC <raulc99g@gmail.com>
2024-12-29 17:24:50 -0600

    Initialize default roles in SecurityConfig using CommandLineRunner

--- src/main/java/com/raulcg/blog/security/SecurityConfig.java
+++ src/main/java/com/raulcg/blog/security/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.raulcg.blog.security;
 
+import com.raulcg.blog.UserRole;
+import com.raulcg.blog.models.Role;
+import com.raulcg.blog.repositories.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,5 +20,19 @@ public class SecurityConfig {
         http.sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+    }
+
+    @Bean
+    public CommandLineRunner initData(RoleRepository roleRepository) {
+
+        return args -> {
+            if (roleRepository.findByRoleName(UserRole.ROLE_ADMIN).isEmpty()) {
+                roleRepository.save(new Role(UserRole.ROLE_ADMIN));
+            }
+
+            if (roleRepository.findByRoleName(UserRole.ROLE_COSTUMER).isEmpty()) {
+                roleRepository.save(new Role(UserRole.ROLE_COSTUMER));
+            }
+        };
     }
 }
