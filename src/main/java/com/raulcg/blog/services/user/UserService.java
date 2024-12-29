bc4e2ec81c94e9caad62473c2490cebfe5739875
Raul99GC <raulc99g@gmail.com>
2024-12-29 17:26:39 -0600

    Add role assignment to user registration from RoleRepository

--- src/main/java/com/raulcg/blog/services/user/UserService.java
+++ src/main/java/com/raulcg/blog/services/user/UserService.java
@@ -1,8 +1,11 @@
 package com.raulcg.blog.services.user;
 
+import com.raulcg.blog.UserRole;
 import com.raulcg.blog.exceptions.EmailAlreadyExistsException;
 import com.raulcg.blog.exceptions.UsernameAlreadyExistsException;
+import com.raulcg.blog.models.Role;
 import com.raulcg.blog.models.User;
+import com.raulcg.blog.repositories.RoleRepository;
 import com.raulcg.blog.repositories.UserRepository;
 import com.raulcg.blog.request.SignupRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +19,9 @@ public class UserService implements IUserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     @Override
     public User registerUser(SignupRequest signupRequest) {
         if (userRepository.existsByEmail(signupRequest.getEmail())) {
@@ -23,10 +29,14 @@ public class UserService implements IUserService {
         } else if (userRepository.existsByUsername(signupRequest.getUsername())) {
             throw new UsernameAlreadyExistsException("Username already taken");
         }
+
+        Role role = roleRepository.findByRoleName(UserRole.ROLE_COSTUMER).orElseThrow(() -> new RuntimeException("Role not found"));
+
         User newUser = new User(signupRequest.getUsername(), signupRequest.getEmail(), signupRequest.getPassword());
         newUser.setFirstName(signupRequest.getFirstName());
         newUser.setLastName(signupRequest.getLastName());
         newUser.setProfileImage(signupRequest.getProfileImage());
+        newUser.setRole(role);
 
         return userRepository.save(newUser);
     }
