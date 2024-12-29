5c47a0da92196f5fc591893983e26b556b99ee42
Raul99GC <raulc99g@gmail.com>
2024-12-28 23:24:00 -0600

    Add UserRole enum

--- src/main/java/com/raulcg/blog/security/SecurityConfig.java
+++ src/main/java/com/raulcg/blog/security/SecurityConfig.java
@@ -1,4 +1,20 @@
 package com.raulcg.blog.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // Permite que los requests que no esten protegidos por Spring Security
+        http.sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
 }
