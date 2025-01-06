f159ff98125a1108b6e2408b96ccae80a8c2a82b
Raul99GC <raulc99g@gmail.com>
2025-01-05 22:07:23 -0600

    Add AuthTokenFilter to SecurityFilterChain configuration

--- src/main/java/com/raulcg/blog/security/SecurityConfig.java
+++ src/main/java/com/raulcg/blog/security/SecurityConfig.java
@@ -3,21 +3,29 @@ package com.raulcg.blog.security;
 import com.raulcg.blog.UserRole;
 import com.raulcg.blog.models.Role;
 import com.raulcg.blog.repositories.RoleRepository;
+import com.raulcg.blog.security.jwt.AuthTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private AuthTokenFilter authJwtTokenFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         // Permite que los requests que no esten protegidos por Spring Security
         http.sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.addFilterBefore(authJwtTokenFilter, BasicAuthenticationFilter.class);
 
         return http.build();
     }
