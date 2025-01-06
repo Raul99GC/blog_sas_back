08d47f5eca0e993fb06ee965535d27fdc08666e9
Raul99GC <raulc99g@gmail.com>
2025-01-05 22:08:34 -0600

    Update SecurityFilterChain to include authorization rules and disable CSRF

--- src/main/java/com/raulcg/blog/security/SecurityConfig.java
+++ src/main/java/com/raulcg/blog/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -25,7 +26,15 @@ public class SecurityConfig {
         // Permite que los requests que no esten protegidos por Spring Security
         http.sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/api/v1/auth/**").permitAll()
+                .requestMatchers("/api/v1/**").authenticated()
+                .anyRequest().permitAll()
+        );
+
         http.addFilterBefore(authJwtTokenFilter, BasicAuthenticationFilter.class);
+
+        http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
